2308c0ff6b1c5e7a355c6aac754d62e0abec2e57
yasser8283 <yassershaikh99@gmail.com>
2026-03-06 16:12:32 +0530

    url fixes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/yasser8283/water_dashboard.git",
+  base: "/water_dashboard/",
   plugins: [react(),tailwindcss()],
 })
